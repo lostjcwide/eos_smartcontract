4100afe9ec904693d5ca79f61c76328ad193e3b5
coreaseo@gmail.com <coreaseo@gmail.com>
2018-08-31 18:56:17 +0900

    up/down:

--- betting/betting.cpp
+++ betting/betting.cpp
@@ -7,6 +7,8 @@
 #include <ctime>
 #include <eosiolib/transaction.hpp>
 #include <eosiolib/crypto.h>
+#include <eosiolib/currency.hpp>
+#include <string.h>
 
 using namespace eosio;
 using namespace std;
@@ -33,7 +35,7 @@ class hello : public eosio::contract {
   public:
       using contract::contract;
 	/// @abi table 
-	struct game
+	struct tgame
 	{
 	    uint64_t  	game_id = 0;
 	    uint64_t  	start_time = 0;
@@ -41,19 +43,20 @@ class hello : public eosio::contract {
 	    asset	bet;
 	    uint64_t	rnd = 0;
 	    uint64_t 	primary_key() const {return game_id;}
-	    EOSLIB_SERIALIZE( game, (game_id)(start_time)(end_time)(bet) )
+	    EOSLIB_SERIALIZE( tgame, (game_id)(start_time)(end_time)(bet)(rnd) )
 
 	};
-	typedef  eosio::multi_index<N(game),game> game_index;
+	typedef  eosio::multi_index<N(tgame),tgame> game_index;
 
       /// @abi action 
       void hi( account_name user ) {
 	curtime();
          print( "Hello, ", name{user} );
       }
-  
+
 	void gameremove(uint64_t game_id)
 	{
+		require_auth(_self);
 		game_index games(_self,_self);
 		auto iter=games.find(1);
 		games.erase(iter);
@@ -62,6 +65,7 @@ class hello : public eosio::contract {
 
 	void gameregist(uint64_t start_time, uint64_t end_time)
 	{
+		require_auth(_self);
 		game_index games(_self,_self);
 		auto iter=games.find(1);
            	if(iter==games.end())
@@ -132,9 +136,106 @@ class hello : public eosio::contract {
 	{
 		game_index games(_self,_self);
 		auto iter=games.find(1);
-		print_f("registed game info : (start_time %, end_time %, rnd %)\t", iter->start_time, iter->end_time, iter->rnd);
+	//	print_f("registed game info : (start_time %, end_time %, rnd %)\t", iter->start_time, iter->end_time, iter->rnd);
 
 	}	
+
+	uint64_t getGameRand()
+	{
+		game_index games(_self,_self);
+		auto iter=games.find(1);
+		return iter->rnd;
+	}
+
+	asset	getBettingToken()
+	{
+		game_index games(_self,_self);
+		auto iter=games.find(1);
+		return iter->bet;
+	}
+
+
+	/// @abi action
+	void distribute( uint64_t receiver, uint64_t code ) {
+		// EOS 토큰이 아닌경우, 이벤트 처리 없이 종료
+		//if(code != N(eosio.token)) return;
+		eosio_assert(code == N(eosio.token), "I reject your non-eosio.token deposit");
+
+		auto data = unpack_action_data<currency::transfer>();
+		if(data.from == _self || data.to != _self) return;
+
+		 eosio_assert(data.quantity.symbol == string_to_symbol(4, "EOS"), "EOS 타입이 아닙니다.");
+		 eosio_assert(data.quantity.is_valid(), "SYMBOL 타입이 맞지 않습니다.");
+		 eosio_assert(data.quantity.amount > 0, " 보낼 수량이 부족합니다.");
+
+
+	}
+
+	//@abi action
+	void exchange( uint64_t receiver, uint64_t code )
+	{
+		uint64_t amount = 0;
+		string sFromAccount = "";
+		string sFromAccountTest = "eos2okayjava";
+		// EOS토큰이 아닌경우, 이벤트 처리 없이 종료
+		if(code != N(eosio.token)) return;
+
+		// #아래 조건문은 EOS가 아닌경우, 트랜잭션을 리젝 시키기 때문에, 다른 토큰을 받을 수가 없음 
+		// eosio_assert(code == N(eosio.token), "I reject your non-eosio.token deposit");
+
+		auto data = unpack_action_data<currency::transfer>();
+		if(data.from == _self || data.to != _self) return;
+
+		amount =  data.quantity.amount/10000;
+
+		if ( getGameRand() == amount )
+		{
+			print_f(",hint : exjectly\t");
+		} else if ( getGameRand() > amount ) {
+			print_f(",hint : down\t");
+		} else if ( getGameRand() < amount ) {
+			print_f(",hint : up\t");
+		}
+
+		// 배팅금액 업데이트
+	        asset tokens;
+		tokens.symbol = string_to_symbol(4, "EOS");
+		tokens.amount = data.quantity.amount;
+		game_index games(_self,_self);
+		games.modify( games.begin(), 0 , [&](auto & ii) {
+			ii.bet += tokens;
+		});
+		print_f(",total Betting: %\t", getBettingToken());
+
+
+	}
+
+
 };
 
-EOSIO_ABI( hello, (hi)(addtime)(nowtime)(gameregist)(gameremove)(gamelist) )
+#define EOSIO_ABI2( TYPE, MEMBERS ) \
+extern "C" { \
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      auto self = receiver; \
+      if( action == N(onerror)) { \
+         /* onerror is only valid if it is for the "eosio" code account and authorized by "eosio"'s "active permission */ \
+         eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
+      } \
+      if( code == self || action == N(onerror) ) { \
+         TYPE thiscontract( self ); \
+         switch( action ) { \
+            EOSIO_API( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } \
+      else { \
+         TYPE thiscontract( self ); \
+         switch( action ) { \
+            case N(transfer): return thiscontract.exchange(receiver, code); \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } \
+   } \
+} \
+
+EOSIO_ABI2( hello, (hi)(addtime)(nowtime)(gameregist)(gameremove)(gamelist)(distribute)(exchange) )
